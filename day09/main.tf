e0ba1d0fe9677a47f5de5ade9338c22e8bc9dec6
MalharKt <malharkauthale2019@gmail.com>
2025-12-21 20:01:12 +0530

    added more eg's  in day 9

--- day09/main.tf
+++ day09/main.tf
@@ -71,3 +71,13 @@
 #     Environment = var.environment
 #   }
 # }
+
+#  resource "aws_instance" "validated_instance" {
+#   ami           = "ami-0f5ee92e2d63afc18"
+#   instance_type = var.instance_type
+
+#   tags = {
+#     Name = "validated-instance"
+#     Type = var.instance_type
+#   }
+# }
--- day09/var.tf
+++ day09/var.tf
@@ -38,3 +38,19 @@
 #   description = "Environment name"
 #   default     = "dev" 
 # }
+
+# variable "instance_type" {
+#   type        = string
+#   description = "EC2 instance type"
+#   default     = "t2.microkvfdmvfmvfmvfmbfbmfk"
+
+#   validation {
+#     condition     = length(var.instance_type) >= 2 && length(var.instance_type) <= 20
+#     error_message = "Instance type must be between 2 and 20 characters"
+#   }
+
+#   validation {
+#     condition     = can(regex("^t[2-3]\\.", var.instance_type))
+#     error_message = "Instance type must start with t2 or t3"
+#   }
+# }
